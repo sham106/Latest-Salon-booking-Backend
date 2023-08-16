cde95aa8c6eeb73e46499056eba14263e7605d88
sham106 <75040811+sham106@users.noreply.github.com>
2023-08-16 09:44:36 +0300

    Update AppointmentController.java

--- src/main/java/com/example/cutesalon/controller/AppointmentController.java
+++ src/main/java/com/example/cutesalon/controller/AppointmentController.java
@@ -10,7 +10,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/appointment")
-@CrossOrigin(origins = "https://cutesalon.vercel.app", allowCredentials = "true")
+@CrossOrigin(origins = "https://cutesalon.vercel.app", allowCredentials = "true", allowedHeaders = "*")
 public class AppointmentController {
     private final AppointmentService appointmentService;
     private final EmailSendServiceImp emailSendServiceImp;
